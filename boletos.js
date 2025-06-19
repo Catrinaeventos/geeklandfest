f0f30e040e475c0cc5c1441392848cd31aa770d3
Catrinaeventos <katycatrinagamingcosplayer@gmail.com>
2025-06-19 00:48:29 -0600

    Add files via upload

---
+++ boletos.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const botonPago = document.querySelector('.boton-pago');
+  const selectTipo = document.getElementById('tipo');
+
+  botonPago.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const tipo = selectTipo.value;
+
+    
+    const links = {
+      'preventa': 'https://mpago.li/1jMDkrG,
+      'Preventa vip': 'https://mpago.la/2Xji7Dh',
+      'Preventa VIP Pass 2 day': 'https://mpago.la/173GLyx',
+      'Ultimate Pass Vip': 'https://mpago.la/32JN1nz',
+    };
+
+    const url = links[tipo];
+
+    if (url) {
+      window.open(url, '_blank');
+    } else {
+      alert('Por favor selecciona un tipo de boleto válido.');
+    }
+  });
+});
